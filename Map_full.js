ec0d2d28192e63184aee7b4621457c66e56e235a
serfegyed <serfegyed@users.noreply.github.com>
2023-10-23 18:10:48 +0200

    refactor: cleaning variables

--- Map_full.js
+++ Map_full.js
@@ -565,38 +565,37 @@ Map.prototype.reduce = function (callback, initialValue) {
  * @param {Object} thisArg - The value to use as `this` when executing the map function.
  * @return {Map} A new Map object containing the mapped key-value pairs.
  */
-Map.from = function (iterable, mapFunc, thisArg) { // ChatGPT refactor
+Map.from = function (iterable, mapFunc, thisArg) { // Bing refactor
     if (iterable == null || typeof iterable !== 'object') {
         throw new TypeError(iterable + " is not an object.");
     }
 
-    if (mapFunc == null) {
-        mapFunc = function (item) { return item; };
-    }
+    mapFunc = mapFunc || function (item) { return item; };
 
     var result = new Map();
+    var entry, elem;
 
     if (iterable instanceof Array) {
         for (var i = 0; i < iterable.length; i++) {
-            var entry = iterable[i];
+            entry = iterable[i];
             if (entry instanceof Array) {
-                var elem = mapFunc.call(thisArg, entry, i);
+                elem = mapFunc.call(thisArg, entry, i);
                 result.set(elem[0], elem[1]);
             }
         }
     } else if (iterable instanceof Map) {
         var iterator = iterable.entries();
-        var entry = iterator.next();
+        entry = iterator.next();
         while (!entry.done) {
-            var elem = mapFunc.call(thisArg, [entry.value[0], entry.value[1]]);
+            elem = mapFunc.call(thisArg, [entry.value[0], entry.value[1]]);
             result.set(elem[0], elem[1]);
             entry = iterator.next();
         }
     } else {
         for (var key in iterable) {
             if (iterable.hasOwnProperty(key)) {
-                var pair = mapFunc.call(thisArg, [key, iterable[key]]);
-                result.set(pair[0], pair[1]);
+                elem = mapFunc.call(thisArg, [key, iterable[key]]);
+                result.set(elem[0], elem[1]);
             }
         }
     }
